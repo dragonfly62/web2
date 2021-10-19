3b5c701258e3541f28ccf5859d514e712e1a7826
sgard62@yahoo.com <anthonysgard@MacBook-Air.local>
2021-10-19 16:19:47 +0200

    Posting with Postman

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/',(req,res) =>{
-    res.send("Hello WEB 2.0");
+/* handling all the parsing */
+app.use(bodyParser.json());
+
+
+app.post('/',(req,res)=>{
+    var email = req.body.email;
+    var amount =req.body.amount;
+
+    res.send({"amount" : amount,"email" : email  });
 });
 
 app.listen(3000,()=>{
