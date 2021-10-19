f36eab1336c779afabe8db230f3de49c6d1cf825
sgard62@yahoo.com <anthonysgard@MacBook-Air.local>
2021-10-19 18:20:14 +0200

    writting your first query

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 var mysql = require('mysql');
 var db_config = {
-    host : '127.0.0.1',
-    user : 'root',
-    password : 'qwerty123456',
+    /*host : '127.0.0.1',*/
+    host : 'localhost',
+    user : 'newuser',
+    password : 'root',
     database : 'webapp'
 }
 
